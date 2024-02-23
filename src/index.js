83778c0089a17ea4643f601a5527fa1761dcf0e3
unknown <mkdimwondu101@gmail.com>
2024-02-23 21:33:50 +0300

    food menu page using react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,98 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./data.js";
+import pizzaData from "./data.js";
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Header() {
+  return (
+    <div className="header footer">
+      <h1>Fast React Pizza Co.</h1>
+    </div>
+  );
+}
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>our menu</h2>
+      <>
+        <p>
+          Authentic Itslian cuisine. 6 creative dishes to choose from. Allfrom
+          our stone oven, all organic, all delichious.
+        </p>
+        <div className="pizzas">
+          {pizzaData.map((pizzza) => (
+            <Pizza pizzaObj={pizzza} />
+          ))}
+        </div>
+      </>
+    </main>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const closedHour = 24;
+  const openHour = 6;
+  const isOpen = hour >= openHour && hour <= closedHour;
+
+  return (
+    // <div className="footer">
+    //   {isOpen > open && isOpen < closed ? (
+    //     <h3> We are Open untile {closed}:00</h3>
+    //   ) : (
+    //     <h3>We are Closed untile {open}:00</h3>
+    //   )}
+    //   <button className="btn">Order</button>
+    // </div>
+    <footer className="footer">
+      {isOpen ? (
+        <Order closedHour={closedHour} />
+      ) : (
+        <div>
+          <p>
+            We are welcome to to serve you {openHour}:00 To{closedHour}:00 use
+            online order only
+          </p>
+          <button className="btn">Order</button>
+        </div>
+      )}
+    </footer>
+  );
+}
+
+function Order({ closedHour }) {
+  return (
+    <div className="order">
+      <p>We are open until {closedHour}:00.come and visit us or order online</p>
+    </div>
+  );
+}
+function Pizza({ pizzaObj }) {
+  return (
+    <div className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+      <div>
+        <h3>{pizzaObj.name}</h3>
+
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : "$" + pizzaObj.price}</span>
+      </div>
+    </div>
+  );
+}
